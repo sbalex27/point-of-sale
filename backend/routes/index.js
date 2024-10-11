f765f0565d1bcaf4014f37fa8f3dc8021a9e5ded
Alex Escobedo <alex2000.sabe@gmail.com>
2024-10-11 16:08:24 -0600

    Soluciona problema instalación

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ title: 'Express' });
+});
+
+router.get('/api', function(req, res, next) {
+  res.json({ name: 'test 1' });
 });
 
 module.exports = router;
