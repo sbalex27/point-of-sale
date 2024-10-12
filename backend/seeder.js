4f8c1d113f3689abec67f5348fdb76065bebc03c
Alex Escobedo <alex2000.sabe@gmail.com>
2024-10-11 18:56:27 -0600

    Error productos solventado

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,15 +1,5 @@
 const fs = require('fs');
-
-const sqlite3 = require('sqlite3').verbose();
-
-// Crear o abrir la base de datos
-const db = new sqlite3.Database('./punto_venta.db', (err) => {
-  if (err) {
-    console.error('Error al abrir la base de datos:', err.message);
-  } else {
-    console.log('Conectado a la base de datos SQLite.');
-  }
-});
+const db = require('./database');
 
 const productos = fs.readFileSync('./seeder/productos.json').toString();
 const productosArray = JSON.parse(productos);
